aceff4837281e253b60124d89717e20a7abbb75e
Carlos Liro <carlos.liro@imr.ie>
2019-04-25 17:41:46 +0200

    feature: material lib elements added

--- projects/ceeliro-amaterial/src/public-api.ts
+++ projects/ceeliro-amaterial/src/public-api.ts
@@ -1,7 +1,4 @@
 /*
  * Public API Surface of ceeliro-amaterial
  */
-
-export * from './lib/ceeliro-amaterial.service';
-export * from './lib/ceeliro-amaterial.component';
 export * from './lib/ceeliro-amaterial.module';
